4bf0b66c18a071470759c09b030547b01c75774f
SMin1620 <ibb09@naver.com>
2022-03-09 21:25:26 +0900

    bind함수 및 setState 함수 이해, 컴포넌트 이벤트(1)

--- src/App.js
+++ src/App.js
@@ -35,22 +35,30 @@ class App extends Component {
     }
     return (
       <div className="App">
-        {/* <Subject 
-          title={this.state.subject.title} sub={this.state.subject.sub}>
-        </Subject> */}
-        <header>
+        <Subject 
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub}
+          onChangePage={function(){
+            this.setState({mode:'welcome'});
+          }.bind(this)}
+          >
+        </Subject>
+        {/* <header>
           <h1><a href="/" onClick={function(e){
             // a 태그를 클릭 시 화면 전환이 되지 않고 값만 출력.
             // event안의 함수에서의 this는 아무것도 가르키고 있지 않다. -> 뒤에 .bind(this)를 추가해야함.
             console.log(e);
             e.preventDefault();
+            // why? 이미 render() 앞에  constructor가 먼저 실행이 됨. 이미 this.state가 선언되었기 때문에 render() 안에서 this.state를 동적으로
+            // 바꾸기 위해서는 this.setState를 사용해야한다.
+            // this.state.mode = 'welcome' --> 이거는 안됨.
             this.setState({
               mode:'welcome'
             });
             // end
           }.bind(this)}>{this.state.subject.title}</a></h1>
           {this.state.subject.sub}
-        </header>
+        </header> */}
         <TOC data={this.state.contents}></TOC>
         <Content title={_title} desc={_desc}></Content>
       </div>
--- src/components/Subject.js
+++ src/components/Subject.js
@@ -5,7 +5,11 @@ class Subject extends Component {
     render(){
       return (
         <header>
-          <h1><a href="/">{this.props.title}</a></h1>
+          {/* onClick 함수를 통해서 onChangePage 함수를 실행. */}
+          <h1><a href="/" onClick={function(e){
+            e.preventDefault();
+            this.props.onChangePage();
+          }.bind(this)}>{this.props.title}</a></h1>
           {this.props.sub}
         </header>
       );
